6e0936e2dbde2f3eb5ac93e54cd50603a586fa84
hamitmizrak <hamitmizrak@gmail.com>
2023-06-03 12:23:01 +0300

    examples

--- src/main/java/com/hamitmizrak/lesson2/_5_Examples1.java
+++ src/main/java/com/hamitmizrak/lesson2/_5_Examples1.java
@@ -15,6 +15,20 @@ public class _5_Examples1 {
         // Dereceyi Fahrenhayt
         // (derece*9/5)+32
         // 0 derece kaç fahranhayttır? (Cevap: 32 fahrenhayttır)
-        //
+
+        // 3.soru iki sayıda dört işlem
+        // x1=4, x2=2;
+        // Toplama =?
+        // Çıkarma =?
+        // Çarpması ?
+        // Bölüm = ?
+        // Bölümünde kalan ?
+
+        // 4.soru VKI
+        // VKI: Vücut Kitle Indeki hesaplayana algoritma yazınız ?
+        // boy=181 CM
+        // kilo: 92 KG
+        // Erkek/ Bayan için Formül: VKI=kilo/boy*boy
+
     }
 }
