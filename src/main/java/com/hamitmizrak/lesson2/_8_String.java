03e2e6fc2c2914566a3f24495c815112406feb2d
hamitmizrak <hamitmizrak@gmail.com>
2023-06-03 14:29:47 +0300

    Conditional

--- src/main/java/com/hamitmizrak/lesson2/_8_String.java
+++ src/main/java/com/hamitmizrak/lesson2/_8_String.java
@@ -35,5 +35,22 @@ public class _8_String {
         System.out.println(temp[1]);
         System.out.println(temp[2]);
 
+        // ÖDEV
+        // Ödev: Kullanıcıdan aldığınız isim ve soyismin; (isimi: scanner ve soyismi: jOptionalPane)
+        // bu kelimenin harf sayısı ? [ ipucu: length() ]
+        // bu kelimenin bütün karakteri büyük yapalım ? [ ipucu: toUpperCase()]
+        // bu kelimenin bütün karakteri küçük yapalım ? [ ipucu: toLowerCase()]
+        // bu kelimenin başında ve sonunda boşlukları alalım ? [ ipucu: trim()]
+        // bu kelimenin j ile başlıyoru mu ?   [ ipucu: startsWith("j")]
+        // bu kelimenin a ile biter mi ?   [ ipucu:  endsWith("a")]
+        // bu kelimenin ilk harfi ? [ ipucu: charAt(0)]
+        // bu kelimenin içinde baştan Altan geçiyor mu ? [ ipucu: indexOf("java")]
+        // bu kelimenin içinde sondan Berkan geçiyor mu ? [ ipucu: lastIndexOf("java")]
+        // bu kelimenin 4.harften sonra gösterilsin ? [ ipucu: substring(4)]
+        // bu kelimenin sonuna INC ekleyelim  ? [ ipucu: concat(".INC")]
+        // bu kelimenin içerisinde java44 geçiyor mu ? [ ipucu: contains("java44")]
+        // bu kelimenin "deneme" ile aynı mı ?  [ipucu: ipucu: equals("deneme");]
+        // equals ile comparateTo arasındaki fark nedir ?
+
     }
 }
