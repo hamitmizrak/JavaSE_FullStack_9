3af58f5a1e493d6e956c36cc1072e1a89402bb99
hamitmizrak <hamitmizrak@gmail.com>
2023-06-03 09:44:01 +0300

    PSVM SOUT

--- src/main/java/com/hamitmizrak/Main.java
+++ src/main/java/com/hamitmizrak/Main.java
@@ -1,4 +1,6 @@
-package com.hamitmizrak;public class Main {
+package com.hamitmizrak;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/com/hamitmizrak/lesson1/PsvmTutorials.java
@@ -0,0 +1,12 @@
+package com.hamitmizrak.lesson1;
+
+// sadece 1 tane public class vardır (inner class hariç)
+// static class yoktur. (inner class hariç)
+public class PsvmTutorials {
+
+    // psvm => TAB
+    public static void main(String[] args) {
+        // sout =>TAB
+        System.out.println("Hello World44");
+    }
+}
