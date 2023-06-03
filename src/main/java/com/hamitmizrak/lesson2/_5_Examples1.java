b579364c3c0f410b31c8228dae1e1826a3790484
hamitmizrak <hamitmizrak@gmail.com>
2023-06-03 12:18:28 +0300

    examples

---
+++ src/main/java/com/hamitmizrak/lesson2/_5_Examples1.java
@@ -0,0 +1,20 @@
+package com.hamitmizrak.lesson2;
+
+public class _5_Examples1 {
+
+    public static void main(String[] args) {
+
+        // 1.soru
+        // 1.dereceden 2 bilinmeyenli denklem
+        // y=3x+4k;
+        // x=5;
+        // k=3
+        // y =?  (Cevap :27)
+
+        // 2.soru
+        // Dereceyi Fahrenhayt
+        // (derece*9/5)+32
+        // 0 derece kaç fahranhayttır? (Cevap: 32 fahrenhayttır)
+        //
+    }
+}
