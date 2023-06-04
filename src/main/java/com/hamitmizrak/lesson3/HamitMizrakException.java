edf96cf7b41cc32b4042b7c6f3962715691bc77a
hamitmizrak <hamitmizrak@gmail.com>
2023-06-04 10:56:23 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/lesson3/HamitMizrakException.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.lesson3;
+
+public class HamitMizrakException extends RuntimeException{
+
+    public HamitMizrakException(String message) {
+        super(message);
+    }
+}
