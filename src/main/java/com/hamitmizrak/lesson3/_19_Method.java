7822e93de5bb97e581a82e23c2af140b4a96250a
hamitmizrak <hamitmizrak@gmail.com>
2023-06-04 12:24:22 +0300

    Araştırma ödevi

--- src/main/java/com/hamitmizrak/lesson3/_19_Method.java
+++ src/main/java/com/hamitmizrak/lesson3/_19_Method.java
@@ -47,6 +47,60 @@ public class _19_Method {
         String str2 = method.parametreliReturnlu("Ankara");
         System.out.println(str2);
 
-
     }
+
+    // ÖDEVLER
+    // Palindrom (Metotla yapıyoruz)
+    // Girilen bir kelimenin tersten okunuşu ve düzden okunuşu aynı ise palindrom denir
+    // Örneğin: ama kelime düzden ve tersten aynı okunuyor.
+
+    // ÖDEV
+    // Asal sayı: eğer bir sayı kendisine ve 1 sayısına bölünüyorsa asal sayıdır
+    // En küçük asal sayı 2 ve aynı zaman sadece 1 tane çift asal sayın vardır.
+    // 2,3,5,7,11 Asal sayıdır. Çünkü sadece 1 ve kendisine bölünür
+    // 4,6,8,9 Asal değilidir.
+    // Örneğin: 9  sayısı ==> 1 ve kendisine (9) bölünür ve 3 sayısınada bölünüyor
+    // asal sayi asal degildir ==> 6/6 6/1 6/2 6/3
+
+    // ARAŞTIRMA:
+    // decoder encoder nedir ?
+    // PasswordEncoder ve Bcrypted nedir ?
+    // Katmanlı mimari nedir ?
+    // Model mapper nedir ?
+    // Swagger nedir ?
+    // Jackson, gson nedir ?
+    // API nedir ?
+    // POJO nedir ?
+    // BEAN nedir ?
+    // overloading nedir ?
+    // override nedir ?
+    // polymorphism nedir ?
+    // encapsulation nedir ?
+    // Gövdeli ve Gövdesiz metot nedir ?
+    // MVC nedir ?
+    // SQL nedir ?
+    // statement - preparementStatement
+
+    // Java 8 gelen özelliklerden
+    // Stream API nedir ?
+    // interface gövdeli metot nedir ?
+    // Optional nedir ?
+
+    // interface nedir ?
+    // abstract nedir ?
+    // inheritance nedir ?
+    // inheritance ile abstract arasındaki fark nedir ?
+    // Serileştirme nedir ?
+
+    // stack hafıza nedir ?
+    // heap hafıza nedir ?
+
+    // Jar nedir ?
+    // war nedir ?
+    // dockerize nedir ?
+    // agile nedir ?
+    // scrum nedir ?
+    // redis nedir ?
+    // graphql nedir ?
+
 }
