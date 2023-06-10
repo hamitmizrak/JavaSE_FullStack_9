d6543f62ab3d3a2c238c31b1318dcc0633168c82
hamitmizrak <hamitmizrak@gmail.com>
2023-06-10 09:47:31 +0300

    Inheritance

--- src/main/java/com/hamitmizrak/lesson5_oop_inheritance/MainOop.java
+++ src/main/java/com/hamitmizrak/lesson5_oop_inheritance/MainOop.java
@@ -2,6 +2,14 @@ package com.hamitmizrak.lesson5_oop_inheritance;
 
 public class MainOop {
     public static void main(String[] args) {
+        Person person=new Person();
+        person.setId(1L);
+        person.setName("person adı");
+        person.setSurname("person soyadı");
+        System.out.println(person);
+        person.fullName();  //polymorphism
+        System.out.println("*************************");
+
         Student student=new Student();
         student.setId(1L);
         student.setName("öğrenci adı");
